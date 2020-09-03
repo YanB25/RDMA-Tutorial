758f8ca980e6da5d4cec7195e7e4fc0d04a2d068
yanbin <yanb20@mails.tsinghua.edu.cn>
2020-09-03 21:54:49 +0800

    change concurrent to 64

--- main.c
+++ main.c
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
     }
 
     config_info.msg_size = 1;
-    config_info.num_concurr_msgs = 128;
+    config_info.num_concurr_msgs = 64;
 
     ret = init_env();
     check(ret == 0, "Failed to init env");
